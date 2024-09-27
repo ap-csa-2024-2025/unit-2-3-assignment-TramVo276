d34ea2f1de139246747e243cc7116d6050c161f4
TramVo276 <tvo941352@gmail.com>
2024-09-27 09:44:17 -0700

    done

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,20 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    public static String firstLastN(String word, int n)
+    {
+      //word = dictionary
+      //n = 3
+      // dicary 
+      String output = new String00(); //makes blank String
+      String firstN = word.substring(0, n); //substring from 0 to n-1
+      int indexOfNthToLast = word.length() - n;
+      String lastN = word.substring(indexOfNthToLast); //strart at nth to last letter, and goes to the end
+
+      output = firstN + lastN;
+      return output;
+    }
+
+
   }
-
-  // write solutions to problems below
-
 }
