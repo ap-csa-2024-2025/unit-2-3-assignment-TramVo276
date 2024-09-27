2ffeb17e7a463f7d494aa37e073331477ea59e0e
TramVo276 <tvo941352@gmail.com>
2024-09-27 09:54:54 -0700

    dictionary

--- Classwork.java
+++ Classwork.java
@@ -9,7 +9,7 @@ public class Classwork
       //word = dictionary
       //n = 3
       // dicary 
-      String output = new String00(); //makes blank String
+      String output = new String(); //makes blank String
       String firstN = word.substring(0, n); //substring from 0 to n-1
       int indexOfNthToLast = word.length() - n;
       String lastN = word.substring(indexOfNthToLast); //strart at nth to last letter, and goes to the end
@@ -17,6 +17,5 @@ public class Classwork
       output = firstN + lastN;
       return output;
     }
-
 
   }
